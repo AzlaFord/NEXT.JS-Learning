3af80630946ae9ca07fc13d29eef3e97b0845bd1
Victor <vbivol394@gmail.com>
2025-06-19 22:07:58 +0300

    fix bugs

--- src/app/api/comments/[id]/route.ts
+++ src/app/api/comments/[id]/route.ts
@@ -11,14 +11,26 @@ export async function GET(
 }
  
 export async function PATCH(
-  request:Request,
-  {params} : {params : { id:string}}
-){
-  const {id} = await params
-  const body = await request.json()
-  const {text} = body
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params; 
+  const body = await request.json();
+  const { text } = body;
 
-  const index = comments.findIndex((comment ) => comment.id === parseInt(id))
-  comments[index].text = text
-  return Response.json(comments[index])
+  const index = comments.findIndex(comment => comment.id === parseInt(id));
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
+}
+
+export async function DELETE(
+  request:Request,
+  {params}:{params:{id:string}}
+){
+  const {id} = params
+  const index = comments.findIndex(comment => comment.id === parseInt(id));
+  const deletedComment = comments[index]
+  comments.splice(index,1)
+  return Response.json(deletedComment)
 }
--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,20 +1,29 @@
 import { comments } from "./data";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(){
-    return Response.json(comments)
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query") || "";
+
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+
+  return NextResponse.json(filteredComments);
 }
 
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   const comment = await request.json();
+
   const newComment = {
     id: comments.length + 1,
-    text: comment.text
+    text: comment.text,
   };
 
   comments.push(newComment);
 
-  return new Response(JSON.stringify(newComment), {
+  return new NextResponse(JSON.stringify(newComment), {
+    status: 201,
     headers: { "Content-Type": "application/json" },
-    status: 201
   });
 }
